854de462bebecc2661f8f2da62c8fd6f86573cb2
waldronmatt <mattwaldron@ymail.com>
2021-12-29 13:26:48 -0500

    compress assets, update serverless config

--- serverless.js
+++ serverless.js
@@ -1,26 +1,21 @@
-import express from 'express';
-import path from 'path';
+const compression = require('compression');
+const express = require('express');
 
 // setup
 const app = express();
 
-// port
-const PORT = process.env.PORT || 8000;
+// compress all responses
+app.use(compression());
 
 // use express.static() to serve files from several directories
 app.use(express.static(__dirname));
 
-// path must route to lambda
-app.use('/.netlify/functions/server', router);
-
-// config
+// serve the index file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`);
-  console.log('Press Ctrl+C to quit.');
-});
+// path must route to lambda
+app.use('/.netlify/functions/server', app);
 
 module.exports.handler = serverless(app);
